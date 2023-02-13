2c15c2ba48648cdfdeebb606e172803fbef8d6cf
alazar-des <alazar.des@gmail.com>
2023-02-13 10:48:21 +0300

    remove icon

--- src/components/PDFGenerator.js
+++ src/components/PDFGenerator.js
@@ -207,13 +207,6 @@ const MyDocument = (props) => (
       </View>
       <View style={styles.contacts}>
         <View style={styles.contact}>
-          <Svg style={styles.icon}>
-            <Path
-              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              stroke="rgb(255, 255, 255)"
-              strokeWidth={1}
-            />
-          </Svg>
           <Text style={styles.contactText}>{props.content.contacts.email}</Text>
         </View>
         <View style={styles.contact}>
